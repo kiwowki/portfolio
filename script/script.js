92795386700e2ff1911ee23cc12c9a36194ec95b
kiwowki <wolves941110@gmail.com>
2023-12-05 19:29:57 +0900

    2023.12.05
    살짝수정

--- script/script.js
+++ script/script.js
@@ -1,5 +1,28 @@
 
 $(function () {
+    // 헤더?
+    // document.addEventListener('DOMContentLoaded', function () {
+    //     const menuItems = document.querySelectorAll('#header .left div');
+    //     const sections = document.querySelectorAll('section');
+
+    //     const observer = new IntersectionObserver(entries => {
+    //         entries.forEach(entry => {
+    //             if (entry.isIntersecting) {
+    //                 // If the section is intersecting with the viewport
+    //                 // Hide all menu items
+    //                 menuItems.forEach(item => item.classList.remove('active'));
+
+    //                 // Show the corresponding menu item
+    //                 const index = Array.from(sections).indexOf(entry.target);
+    //                 menuItems[index + 1].classList.add('active');
+    //             }
+    //         });
+    //     }, { threshold: 0.5 }); // Adjust the threshold as needed
+
+    //     // Observe each section
+    //     sections.forEach(section => observer.observe(section));
+    // });
+
     // 전역 스코프에서 flipCard 함수 정의
     function flipCard(element) {
         const flipper = element.querySelector('.flipper');
@@ -11,6 +34,31 @@ $(function () {
         $('.flip-container').click(function () {
             // flipCard 함수 호출
             flipCard(this);
+        });
+    });
+
+    document.addEventListener('DOMContentLoaded', function () {
+        const container = document.querySelector('.background-container');
+        const copies = document.querySelectorAll('.background-copy');
+
+        // Number of copies
+        const numCopies = copies.length;
+
+        window.addEventListener('scroll', function () {
+            // 현재 스크롤 위치 구하기
+            const scrollPosition = window.scrollY;
+
+            // 스크롤 위치에 따라 특정 비율의 이미지를 보여주기
+            for (let i = 0; i < numCopies; i++) {
+                if (i === 0) {
+                    // nth-child(1)일 경우에는 고정으로 나오도록 설정
+                    copies[i].style.opacity = 1;
+                } else {
+                    // 다른 이미지들은 스크롤 위치에 따라 특정 비율의 효과 적용
+                    const percentage = ((scrollPosition - copies[i].offsetTop) / window.innerHeight) * 100;
+                    copies[i].style.opacity = Math.max(0, Math.min(1, (percentage - (i - 1) * 20) / 20));
+                }
+            }
         });
     });
 })
