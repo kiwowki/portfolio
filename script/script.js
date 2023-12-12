b8a3c2a383e254fab5043a04c96e235daa3f5116
kiwowki <wolves941110@gmail.com>
2023-12-12 16:12:30 +0900

    2023.12.12

--- script/script.js
+++ script/script.js
@@ -17,19 +17,21 @@ $(document).ready(function () {
     });
 
     gsap.set(".intro_title .title .title_t1, .intro_title .title .title_t2", { overflow: "hidden" });
-    gsap.set(".intro_title .title .title_t1 p", { opacity: 0, translateY: 500, z: -1000 });
-    gsap.set(".intro_title .title .title_t2 p", { opacity: 0, translateY: -500, z: -1000 });
-    gsap.set(".images_wrap .flip-container", { scale: 2, opacity: 0 });
+    gsap.set(".intro_title .title .title_t1 p", { opacity: 0, y: 100, z: -1000 });
+    gsap.set(".intro_title .title .title_t2 p", { opacity: 0, y: -100, z: -1000 });
+    gsap.set(".images_wrap .flip-container", { opacity: 0, scale: 2.5 });
     gsap.set("#header", { y: -500 });
 
     let imageElements = document.querySelectorAll(".images_wrap .flip-container");
     let shuffledImages = Array.from(imageElements).sort(() => Math.random() - 0.5);
 
-    let tl = gsap.timeline();
+    setTimeout(() => {
+        let tl = gsap.timeline();
 
-    tl.to(".intro_title .title .title_t1 p, .intro_title .title .title_t2 p", { duration: 0.7, opacity: 1, translateY: 0, stagger: 0.5 });
-    tl.to(shuffledImages, { duration: 0.3, opacity: 1, scale: 1, stagger: 0.2 });
-    tl.to("#header", { duration: 1.2, y: 0 });
+        tl.to(".intro_title .title .title_t1 p, .intro_title .title .title_t2 p", { opacity: 1, y: 0, duration: 0.4, ease: "power2.out" }, "a");
+        tl.to(shuffledImages, { duration: 0.2, opacity: 1, scale: 1, stagger: 0.2 });
+        tl.to("#header", { duration: 1.2, y: 0 });
+    }, 2000);
 
     // intro 이미지 뒤집으면 글자 나오게
     function flipCard(element) {
@@ -41,8 +43,15 @@ $(document).ready(function () {
         // flipCard 함수 호출
         flipCard(this);
     });
+
+
+
+
+
 });
 
+
+// header
 // 스크롤 위치에 따른 헤더 메뉴 업데이트 함수
 function updateHeaderVisibility() {
     const headerLeft = document.querySelector('.left');
@@ -75,6 +84,21 @@ function updateHeaderVisibility() {
     }
 }
 
+// 헤더 숨기기 효과
+const showNav = gsap.from("#header", {
+    yPercent: -200,
+    paused: true,
+    duration: 0.8
+}).progress(1);
+
+ScrollTrigger.create({
+    start: "top top",
+    end: 99999,
+    onUpdate: (self) => {
+        self.direction === -1 ? showNav.play() : showNav.reverse()
+    }
+});
+
 const container = document.querySelector('.background-container');
 const copies = document.querySelectorAll('.background-copy');
 
@@ -91,9 +115,92 @@ window.addEventListener('scroll', function () {
             // nth-child(1)일 경우에는 고정으로 나오도록 설정
             copies[i].style.opacity = 1;
         } else {
+            const offsetAdjustment = 600;
+
             // 다른 이미지들은 스크롤 위치에 따라 특정 비율의 효과 적용
-            const percentage = ((scrollPosition - copies[i].offsetTop) / window.innerHeight) * 100;
-            copies[i].style.opacity = Math.max(0, Math.min(1, (percentage - (i - 1) * 20) / 20));
+            const percentage = ((scrollPosition - (copies[i].offsetTop + offsetAdjustment)) / window.innerHeight) * 100;
+            copies[i].style.opacity = Math.max(0, Math.min(1, (percentage - (i - 1) * 10) / 10));
         }
     }
 });
+
+
+// about_text_top
+// about_text_top 글자 애니메이션
+const targetsT1 = gsap.utils.toArray(".about_text_top .t1.split");
+const targetsT2 = gsap.utils.toArray(".about_text_top .t2.split");
+const targetsT3 = gsap.utils.toArray(".about_text_top .t3.split");
+
+targetsT1.forEach(target => {
+    let splitClient = new SplitType(target, { type: "lines, words, chars" });
+    let chars = splitClient.chars;
+
+    gsap.from(chars, {
+        yPercent: 50,
+        opacity: 0,
+        rotation: 10,
+        duration: 0.7,
+        stagger: 0.1,
+        scrollTrigger: {
+            trigger: target,
+            start: "top bottom",
+            end: "+400",
+            markers: true
+        }
+    });
+});
+targetsT2.forEach(target => {
+    let splitClient = new SplitType(target, { type: "lines, words, chars" });
+    let chars = splitClient.chars;
+
+    gsap.from(chars, {
+        yPercent: 50,
+        opacity: 0,
+        rotation: 10,
+        duration: 0.7,
+        stagger: 0.2,
+        scrollTrigger: {
+            trigger: target,
+            start: "top bottom",
+            end: "+400",
+            markers: true
+        }
+    });
+});
+targetsT3.forEach(target => {
+    let splitClient = new SplitType(target, { type: "lines, words, chars" });
+    let chars = splitClient.chars;
+
+    gsap.from(chars, {
+        yPercent: 50,
+        opacity: 0,
+        rotation: 10,
+        duration: 0.7,
+        stagger: 0.3,
+        scrollTrigger: {
+            trigger: target,
+            start: "top bottom",
+            end: "+400",
+            markers: true
+        }
+    });
+});
+
+// about photos 2 랜덤이미지
+// 이미지 파일 이름을 배열에 저장합니다.
+const imageArray = ["img/about_img2.jpg", "img/about_img3.jpg", "img/about_img4.jpg"];
+
+// 모든 photo 클래스를 가진 div 요소를 선택합니다.
+const photoElements = document.querySelectorAll(".p2 .about_photos_img .photo");
+
+// 각 요소에 대해 랜덤한 이미지를 적용합니다.
+photoElements.forEach((photoElement) => {
+    // 랜덤한 숫자를 생성하여 배열에서 이미지를 선택합니다.
+    const randomIndex = Math.floor(Math.random() * imageArray.length);
+    const randomImage = imageArray[randomIndex];
+
+    // 선택된 이미지를 배경으로 설정합니다.
+    photoElement.style.backgroundImage = `url('${randomImage}')`;
+});
+
+
